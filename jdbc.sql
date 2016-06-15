507b48ef854cd3d58431453648650ba9ad26584b
minsu <minsu.kim08@gmail.com>
2016-06-15 10:20:49 +0900

    add SQL file with create table

---
+++ jdbc.sql
@@ -0,0 +1,14 @@
+create table education(
+	seq number primary key,
+	title varchar(30) not null,
+	url varchar(30) not null);
+	
+create table kpop(
+	seq number primary key,
+	title varchar(30) not null,
+	url varchar(30) not null);
+	
+create table travel(
+	seq number primary key,
+	title varchar(30) not null,
+	url varchar(30) not null);
